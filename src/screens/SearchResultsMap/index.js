b989f912acb36513f2652f35f28b5679086db48a
jeff graham <jeffery.lemar.graham@gmail.com>
2023-02-02 00:32:42 -0500

    implement map on ios

--- src/screens/SearchResultsMap/index.js
+++ src/screens/SearchResultsMap/index.js
@@ -1,10 +1,19 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import MapView from 'react-native-maps'
 
 const SearchResultsMap = () => {
   return (
-    <View>
-      <Text>SearchResultsMap</Text>
+    <View style={{width: '100%', height: '100%'}}>
+      <MapView
+        style={{width: '100%', height: '100%'}}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   )
 }
