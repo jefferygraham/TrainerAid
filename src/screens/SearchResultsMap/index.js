8993b155703e461f39e1130d007dbb0b4c5e8812
jeff graham <jeffery.lemar.graham@gmail.com>
2023-02-01 23:55:57 -0500

    add screen for maps

---
+++ src/screens/SearchResultsMap/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SearchResultsMap = () => {
+  return (
+    <View>
+      <Text>SearchResultsMap</Text>
+    </View>
+  )
+}
+
+export default SearchResultsMap
