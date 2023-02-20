1eb889523d34380c6b3a17aa0b095d6cd88e6503
jeff graham <jeffery.lemar.graham@gmail.com>
2023-02-19 19:23:12 -0500

    refactor calendar

---
+++ constants/style.js
@@ -0,0 +1,168 @@
+export const Fonts = {
+    whiteColor10Medium: {
+      fontSize: 10.0,
+      color: '#FFFFFF',
+    },
+  
+    whiteColor14Medium: {
+      fontSize: 14.0,
+      color: '#FFFFFF',
+    },
+  
+    whiteColor16Medium: {
+      fontSize: 16.0,
+      color: '#FFFFFF',
+    },
+  
+    whiteColor18Medium: {
+      fontSize: 18.0,
+      color: '#FFFFFF',
+    },
+  
+    whiteColor48Medium: {
+      fontSize: 48.0,
+      color: '#FFFFFF',
+    },
+  
+    whiteColor14Bold: {
+      fontSize: 14.0,
+      color: '#FFFFFF',
+    },
+  
+    whiteColor18Bold: {
+      fontSize: 18.0,
+      color: '#FFFFFF',
+    },
+  
+    whiteColor36Bold: {
+      fontSize: 36.0,
+      color: '#FFFFFF',
+    },
+  
+    blackColor12Regular: {
+      fontSize: 12.0,
+      color: '#000000',
+    },
+  
+    blackColor14Regular: {
+      fontSize: 14.0,
+      color: '#000000',
+    },
+  
+    blackColor14Medium: {
+      fontSize: 14.0,
+      color: '#000000',
+    },
+  
+    blackColor16Medium: {
+      fontSize: 16.0,
+      color: '#000000',
+    },
+  
+    blackColor18Medium: {
+      fontSize: 18.0,
+      color: '#000000',
+    },
+  
+    blackColor16SemiBold: {
+      fontSize: 16.0,
+      color: '#000000',
+    },
+  
+    blackColor14Bold: {
+      fontSize: 14.0,
+      color: '#000000',
+    },
+  
+    blackColor16Bold: {
+      fontSize: 16.0,
+      color: '#000000',
+    },
+  
+    blackColor18Bold: {
+      fontSize: 18.0,
+      color: '#000000',
+    },
+  
+    primaryColor12Regular: {
+      fontSize: 12.0,
+      color: '#DB1818',
+    },
+  
+    primaryColor14Regular: {
+      fontSize: 14.0,
+      color: '#ff7c00',
+    },
+  
+    primaryColor14Medium: {
+      fontSize: 14.0,
+      color: '#ff7c00',
+    },
+  
+    primaryColor18Bold: {
+      fontSize: 18.0,
+      color: '#ff7c00',
+    },
+  
+    primaryColor22Bold: {
+      fontSize: 22.0,
+      color: '#ff7c00',
+    },
+  
+    grayColor12Regular: {
+      fontSize: 12.0,
+      color: '#CFC6C6',
+    },
+  
+    grayColor12MediumItalic: {
+      fontSize: 12.0,
+      color: '#CFC6C6',
+      fontStyle: 'italic',
+    },
+  
+    grayColor10Medium: {
+      fontSize: 10.0,
+      color: '#CFC6C6',
+    },
+  
+    grayColor12Medium: {
+      fontSize: 12.0,
+      color: '#CFC6C6',
+    },
+  
+    grayColor14Medium: {
+      fontSize: 14.0,
+      color: '#CFC6C6',
+    },
+  
+    grayColor16Medium: {
+      fontSize: 16.0,
+      color: '#CFC6C6',
+    },
+  
+    grayColor18Bold: {
+      fontSize: 18.0,
+      color: '#CFC6C6',
+    },
+  
+    grayColor20Bold: {
+      fontSize: 20.0,
+      color: '#CFC6C6',
+    },
+  };
+  
+  export const Colors = {
+    whiteColor: '#FFFFFF',
+    blackColor: '#000000',
+    primaryColor: '#ff7c00',
+    ratingColor: '#BFDC0F',
+    grayColor: '#CFC6C6',
+  };
+  
+  export const Sizes = {
+    fixPadding: 10.0,
+  };
+  
+  const appStyle = { Fonts, Sizes, Colors };
+  
+  export default appStyle;
--- src/screens/Appointments/index.js
+++ src/screens/Appointments/index.js
@@ -1,37 +1,252 @@
-import React, { useState } from 'react'
-import { Button } from 'react-native'
-import DatePicker from 'react-native-date-picker'
-import { useNavigation } from "@react-navigation/native";
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  View,
+  StatusBar,
+  StyleSheet,
+  Dimensions,
+  Text,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+
+import CalendarStrip from 'react-native-calendar-strip';
+import Dialog from 'react-native-dialog';
+import { Colors, Sizes, Fonts } from '../../../constants/style';
 
 
-const Appointments = () => {
-  const [date, setDate] = useState(new Date())
-  const [open, setOpen] = useState(false)
-  const navigation = useNavigation()
+const { width } = Dimensions.get('window');
+
+const slots = [
+  '9:00 AM',
+  '10:00 AM',
+  '11:00 AM',
+  '12:00 PM',
+  '1:00 PM',
+  '2:00 PM',
+  '3:00 PM',
+  '4:00 PM',
+  '5:00 PM',
+  '6:00 PM',
+  '7:00 PM',
+];
+
+const Appointments = ({ navigation }) => {
+  const [state, setState] = useState({
+    selectedSlot: slots[0],
+    showSuccessDialog: false,
+  });
+
+  const updateState = (data) => setState((state) => ({ ...state, ...data }));
+
+  const { selectedSlot, showSuccessDialog } = state;
 
   return (
-    <>
-      <Button title="Pick a date & time" onPress={() => setOpen(true)} />
-      <DatePicker
-        modal
-        minuteInterval={15}
-        open={open}
-        date={date}
-        onConfirm={(date) => {
-          setOpen(false)
-          setDate(date)
+    <SafeAreaView style={{ flex: 1, backgroundColor: Colors.whiteColor }}>
+      <StatusBar translucent={false} backgroundColor={Colors.primaryColor} />
+      <View style={{ flex: 1 }}>
+        {header()}
+        {calender()}
+        {divider()}
+        {slotInfo()}
+        {continueButton()}
+        {successDialog()}
+      </View>
+    </SafeAreaView>
+  );
 
-          navigation.navigate('Confirmation', {
-            date: date.toLocaleDateString(),
-            time: date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
-          })
+  function successDialog() {
+    return (
+      <Dialog.Container
+        visible={showSuccessDialog}
+        contentStyle={styles.dialogWrapStyle}
+        headerStyle={{ margin: 0.0 }}
+      >
+        <View
+          style={{ backgroundColor: Colors.whiteColor, alignItems: 'center' }}
+        >
+          <View style={styles.successIconWrapStyle}>
+            {/* <MaterialIcons name='done' size={24} color={Colors.primaryColor} /> */}
+          </View>
+          <Text
+            style={{ ...Fonts.grayColor18Bold, marginTop: Sizes.fixPadding }}
+          >
+            Success
+          </Text>
+        </View>
+      </Dialog.Container>
+    );
+  }
+
+  function continueButton() {
+    return (
+      <TouchableOpacity
+        activeOpacity={0.9}
+        onPress={() => {
+          navigation.push('SelectPaymentMethod');
         }}
-        onCancel={() => {
-          setOpen(false)
+        style={styles.continueButtonStyle}
+      >
+        <Text style={{ ...Fonts.whiteColor18Bold }}>Continue</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  function slotInfo() {
+    const renderItem = ({ item, index }) => {
+      return (
+        <TouchableOpacity
+          activeOpacity={0.9}
+          onPress={() => {
+            updateState({ selectedSlot: item });
+          }}
+        >
+          <View
+            style={{
+              backgroundColor:
+                selectedSlot == item ? Colors.primaryColor : Colors.whiteColor,
+              borderColor:
+                selectedSlot == item ? Colors.primaryColor : '#D7D7D7',
+              ...styles.slotWrapStyle,
+            }}
+          >
+            <Text
+              style={
+                selectedSlot == item
+                  ? { ...Fonts.whiteColor14Medium }
+                  : { ...Fonts.primaryColor14Medium }
+              }
+            >
+              {item}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      );
+    };
+
+    return (
+      <View>
+        <Text
+          style={{
+            ...Fonts.blackColor16Bold,
+            marginVertical: Sizes.fixPadding + 5.0,
+            marginHorizontal: Sizes.fixPadding * 2.0,
+          }}
+        >
+          {slots.length} Slots
+        </Text>
+        <FlatList
+          data={slots}
+          keyExtractor={(index) => `${index}`}
+          renderItem={renderItem}
+          numColumns={3}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            paddingLeft: Sizes.fixPadding * 2.0,
+          }}
+        />
+      </View>
+    );
+  }
+
+  function divider() {
+    return <View style={styles.dividerStyle} />;
+  }
+
+  function calender() {
+    const datesBlacklistFunc = (date) => {
+      return date.isoWeekday() === 7;
+    };
+
+    return (
+      <CalendarStrip
+        calendarAnimation={{ type: 'sequence', duration: 30 }}
+        daySelectionAnimation={{
+          type: 'background',
+          duration: 200,
+          highlightColor: Colors.primaryColor,
         }}
+        style={{ height: 100, paddingTop: 20, paddingBottom: 10 }}
+        calendarHeaderStyle={{
+          color: Colors.blackColor,
+          marginBottom: Sizes.fixPadding + 5.0,
+        }}
+        calendarColor={'transparent'}
+        dateNumberStyle={{ ...Fonts.blackColor18Medium }}
+        dateNameStyle={{ ...Fonts.blackColor16Medium }}
+        highlightDateNumberStyle={{ ...Fonts.whiteColor18Medium }}
+        highlightDateNameStyle={{ ...Fonts.whiteColor16Medium }}
+        scrollable={true}
+        upperCaseDays={false}
+        datesBlacklist={datesBlacklistFunc}
+        disabledDateNameStyle={{ ...Fonts.grayColor14Medium }}
+        disabledDateNumberStyle={{ ...Fonts.grayColor14Medium }}
+        useIsoWeekday={false}
       />
-    </>
-  )
-}
+    );
+  }
 
-export default Appointments
+  function header() {
+    return (
+      <View style={styles.headerWrapStyle}>
+        {/* <MaterialIcons
+          name='arrow-back'
+          size={24}
+          color={Colors.blackColor}
+          onPress={() => navigation.pop()}
+          style={{ position: 'absolute', left: 20.0 }}
+        /> */}
+        <Text
+          style={{
+            ...Fonts.blackColor18Bold,
+            marginLeft: Sizes.fixPadding + 5.0,
+          }}
+        >
+          Select date & time
+        </Text>
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  headerWrapStyle: {
+    backgroundColor: Colors.whiteColor,
+    height: 50.0,
+    elevation: 3.0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: Sizes.fixPadding * 2.0,
+  },
+  slotWrapStyle: {
+    alignItems: 'center',
+    borderRadius: Sizes.fixPadding - 5.0,
+    marginBottom: Sizes.fixPadding * 2.0,
+    justifyContent: 'center',
+    borderWidth: 1.0,
+    elevation: 3.0,
+    marginRight: Sizes.fixPadding * 2.0,
+    height: 30.0,
+    minWidth: width / 3.9,
+    flex: 1,
+  },
+  dividerStyle: {
+    backgroundColor: Colors.grayColor,
+    height: 1.0,
+    marginVertical: Sizes.fixPadding,
+    marginHorizontal: Sizes.fixPadding * 2.0,
+  },
+  continueButtonStyle: {
+    position: 'absolute',
+    bottom: 0.0,
+    left: 0.0,
+    right: 0.0,
+    backgroundColor: Colors.primaryColor,
+    height: 50.0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Appointments;
